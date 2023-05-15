2a0f69b0158d76970f2fd3e72a543072c81f1da4
Enoch Taylor <tay121222@gmail.com>
2023-05-15 11:58:54 +0000

    beautiful code that passes the Betty checks

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry Point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		printf("Hello, World!\n");
+	}
+
+	return (0);
+}
